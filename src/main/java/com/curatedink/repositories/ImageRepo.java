aa6aa9fe6d3dd68921bc86594b6d4f4347deb1a0
Michael Baker <michael.a.baker34@gmail.com>
2021-05-17 16:02:50 -0500

    add image repo

---
+++ src/main/java/com/curatedink/repositories/ImageRepo.java
@@ -0,0 +1,7 @@
+package com.curatedink.repositories;
+
+import com.curatedink.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepo extends JpaRepository<Image, Long> {
+}
