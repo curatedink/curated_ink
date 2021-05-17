41d3a28c4d39e117211943f9d6f62b3e0ab52f2e
Michael Baker <michael.a.baker34@gmail.com>
2021-05-17 09:26:25 -0500

    initial commit

---
+++ src/main/java/com/curatedink/CuratedinkApplication.java
@@ -0,0 +1,13 @@
+package com.curatedink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratedinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuratedinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curatedink/CuratedinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curatedink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuratedinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
