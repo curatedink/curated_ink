b6efc340ae701ac602cd571a7b99e5360f8a705e
Corey Koontz <corey.michael.koontz@gmail.com>
2021-05-17 15:35:03 -0500

    Feat: Adding style model

---
+++ src/main/java/com/curatedink/models/Style.java
@@ -0,0 +1,44 @@
+package com.curatedink.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "styles")
+public class Style {
+
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto incrementing
+    @Column(columnDefinition = "INT UNSIGNED")
+    private long id;
+
+    @Column(nullable = false)
+    private String style;
+
+    // ------------------------------------------------------ Constructors:
+
+    public Style() {
+    }
+
+    public Style(String style) {
+        this.style = style;
+    }
+
+    // ------------------------------------------------------ Getters & Setters:
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
