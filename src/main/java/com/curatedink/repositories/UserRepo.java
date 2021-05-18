e699836b8aea27361402ab8dd6b575c8180a7fb0
Michael Baker <michael.a.baker34@gmail.com>
2021-05-18 10:14:51 -0500

    add userRepo

---
+++ src/main/java/com/curatedink/repositories/UserRepo.java
@@ -0,0 +1,7 @@
+package com.curatedink.repositories;
+
+import com.curatedink.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+}
