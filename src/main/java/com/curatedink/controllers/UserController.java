bc06c4008baa23c68f2bd3c4a51ff762663e1c00
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-18 09:31:43 -0500

    updating with buildable

--- src/main/java/com/curatedink/controllers/UserController.java
+++ src/main/java/com/curatedink/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.curatedink.controllers;
 
 import com.curatedink.models.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
