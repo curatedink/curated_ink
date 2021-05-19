97c8e8eec04db52db86c68b43f04875dafaffd9c
Corey Koontz <corey.michael.koontz@gmail.com>
2021-05-19 12:27:07 -0500

    Adding CRUD for create and adding a post map for testing login

--- src/main/java/com/curatedink/controllers/AuthenticationController.java
+++ src/main/java/com/curatedink/controllers/AuthenticationController.java
@@ -1,13 +1,45 @@
 package com.curatedink.controllers;
 
+import com.curatedink.models.User;
+import com.curatedink.repositories.UserRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthenticationController {
+    public AuthenticationController(UserRepo userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/login")
     public String showLoginForm() {
         return "tattoos/login";
     }
 
+    private final UserRepo userDao;
+
+
+    // ----------------- For Testing Only. Remove below when setting up spring security
+
+    // Test Artist Profile:
+    @PostMapping("/login")
+    public String login(Model model, @RequestParam String username, @RequestParam String password) {
+        User user = userDao.findByUsername(username);
+        System.out.println(user.getDisplayName());
+        model.addAttribute("user", user);
+        boolean userType = user.getIsArtist();
+        if (userType) {
+            return "users/artist-profile";
+        } else {
+            return "users/canvas-profile";
+        }
+    }
+
+
+    // ----------------------------------> Remove the above when implementing security
 }
+
+
--- src/main/java/com/curatedink/repositories/ImagesRepo.java
+++ src/main/java/com/curatedink/repositories/ImagesRepo.java
@@ -3,6 +3,6 @@ package com.curatedink.repositories;
 import com.curatedink.models.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImagesDao extends JpaRepository<Image, Long> {
+public interface ImagesRepo extends JpaRepository<Image, Long> {
 
 }
