4c89ef5de06c46248aa2248203058dfd6ee975e8
javier estrada <estrada.javier.2022@gmail.com>
2021-05-24 16:44:39 -0500

    feat saving progress to my controller

--- src/main/java/com/curatedink/controllers/EmailController.java
+++ src/main/java/com/curatedink/controllers/EmailController.java
@@ -2,6 +2,7 @@ package com.curatedink.controllers;
 
 import com.curatedink.models.Image;
 import com.curatedink.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmailController {
-    @GetMapping("/send-email")
+
+//@Autowired
+//EmailService emailService;
+
+@GetMapping("/send-email")
     public String welcome() {
 
         return "";
     }
 
 @PostMapping("/send-email")
-public String insert() {
+    public String insert() {
 
     return "";
     }
