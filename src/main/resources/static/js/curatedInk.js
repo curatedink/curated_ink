d0b1122b2ff7f55c9acf674b4850f925e9a4f37c
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-26 17:40:22 -0500

    Saving work so far on creating modal with changes

--- src/main/resources/static/js/curatedInk.js
+++ src/main/resources/static/js/curatedInk.js
@@ -1,8 +1,8 @@
 
 (function ($) {
-    var request = $.ajax({'url': '/gallery.json'});
+    let request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
-        var html = '';
+        let html = '';
         images.forEach(function (image) {
             html += '<div class="card m-5" style="width: 24rem;">';
             html += '<div class="card-body">';
@@ -17,19 +17,48 @@
 })(jQuery);
 
 (function ($) {
-    var request = $.ajax({'url': '/gallery.json'});
+    let request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
-        var html = '';
+        let html = '';
         images.forEach(function (image) {
             html += '<div class="card m-5" style="width: 24rem;">';
             html += '<div class="card-body">';
             html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
             html += '</div>';
-            html += '<a class="btn" id="detailed-work" data-index-number=${image.id} data-target="#exampleModal"' +
+            html += '<a class="btn" id="detailed-work" class="identifyingClass" ' +
+                ' data-index-number=${image.id} data-target="#exampleModal"' +
                 ' data-toggle="modal">View' +
                 ' Details</a>'
             html += '</div>';
         });
         $('#curated-gallery').html(html);
+    });
+})(jQuery);
+
+// console.log(images);
+
+function showTattooDetails(images) {
+    let html = '<div>';
+    html += '<h2>' + image.imageUrl + '</h2>';
+    html += '<p class="ml-2">' + image.creditedArtist + '</p>';
+    html += '</div>';
+    return html;
+}
+
+(function ($) {
+    let request = $.ajax({'url': '/gallery.json'});
+    request.done(function (images) {
+        let html = '';
+        images.forEach(function (image) {
+            html += '<div class="card m-5" style="width: 24rem;">';
+            html += '<div class="card-body">';
+            html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
+            html += '</div>';
+            html += '</div>';
+        });
+        $('#detailed-work').on('click', function() {
+            $(this).html(html);
+        });
+        // $('#detailed-work').on(html);
     });
 })(jQuery);
