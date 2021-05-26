8b1cd1469a4eae85de89957bd02b88df1633f3b5
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-26 16:18:12 -0500

    Added back modal as test

--- src/main/resources/static/js/curatedInk.js
+++ src/main/resources/static/js/curatedInk.js
@@ -1,29 +1,33 @@
 
-
-(function($) {
+(function ($) {
     var request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
         var html = '';
-        images.forEach(function(image) {
-            html += '<div>';
-            html += '<h1>' + image.creditedArtist + '</h1>';
-            html += '<p>' + image.studioName + '</p>';
-            // html += '<p>Published by ' + ad.owner.username + '</p>';
+        images.forEach(function (image) {
+            html += '<div class="card m-5" style="width: 24rem;">';
+            html += '<div class="card-body">';
+            html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
+            html += '</div>';
+            html += '<a class="btn" id="detailed-work" data-index-number=${image.id} data-toggle="modal">View' +
+                ' Details</a>'
             html += '</div>';
         });
         $('#images').html(html);
     });
 })(jQuery);
 
-(function($) {
+(function ($) {
     var request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
         var html = '';
-        images.forEach(function(image) {
-            html += '<div>';
-            html += '<h1>' + image.imageUrl + '</h1>';
-            // html += '<p>' + image.studioName + '</p>';
-            // html += '<p>Published by ' + ad.owner.username + '</p>';
+        images.forEach(function (image) {
+            html += '<div class="card m-5" style="width: 24rem;">';
+            html += '<div class="card-body">';
+            html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
+            html += '</div>';
+            html += '<a class="btn" id="detailed-work" data-index-number=${image.id} data-target="#exampleModal"' +
+                ' data-toggle="modal">View' +
+                ' Details</a>'
             html += '</div>';
         });
         $('#curated-gallery').html(html);
