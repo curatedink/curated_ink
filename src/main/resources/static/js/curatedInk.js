095493fb6a159a4486424235e35d2e83a6521aa2
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-26 18:13:27 -0500

    Added function to pass data through to modal when view more is clicked in gallery views

--- src/main/resources/static/js/curatedInk.js
+++ src/main/resources/static/js/curatedInk.js
@@ -8,7 +8,8 @@
             html += '<div class="card-body">';
             html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
             html += '</div>';
-            html += '<a class="btn" id="detailed-work" data-index-number=${image.id} data-toggle="modal">View' +
+            html += '<a class="btn" data-index-number=${image.id} data-target="#gallery-detailed"' +
+                ' data-toggle="modal">View' +
                 ' Details</a>'
             html += '</div>';
         });
@@ -25,8 +26,8 @@
             html += '<div class="card-body">';
             html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
             html += '</div>';
-            html += '<a class="btn" id="detailed-work" class="identifyingClass" ' +
-                ' data-index-number=${image.id} data-target="#exampleModal"' +
+            html += '<a class="btn" ' +
+                ' data-index-number=${image.id} data-target="#gallery-detailed"' +
                 ' data-toggle="modal">View' +
                 ' Details</a>'
             html += '</div>';
@@ -37,28 +38,19 @@
 
 // console.log(images);
 
-function showTattooDetails(images) {
-    let html = '<div>';
-    html += '<h2>' + image.imageUrl + '</h2>';
-    html += '<p class="ml-2">' + image.creditedArtist + '</p>';
-    html += '</div>';
-    return html;
-}
 
 (function ($) {
     let request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
         let html = '';
         images.forEach(function (image) {
-            html += '<div class="card m-5" style="width: 24rem;">';
-            html += '<div class="card-body">';
+            html += '<div class="m-5" style="width: 24rem;">';
+            html += '<div class="">';
             html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
+            html += '<p class="ml-2">' + image.creditedArtist + '</p>';
             html += '</div>';
             html += '</div>';
         });
-        $('#detailed-work').on('click', function() {
-            $(this).html(html);
-        });
-        // $('#detailed-work').on(html);
+        $('#detailed-work').html(html);
     });
 })(jQuery);
