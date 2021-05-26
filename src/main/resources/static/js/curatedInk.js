e600a318e743cb0da322b136897fcbf8688078f9
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-26 15:22:53 -0500

    Added tab panel for gallery views

--- src/main/resources/static/js/curatedInk.js
+++ src/main/resources/static/js/curatedInk.js
@@ -1,24 +1,5 @@
 
 
-// function getImagesJSON(){
-//     var images = null;
-//
-//     $.ajax({
-//         url: "/gallery.json",
-//         method: "GET",
-//         async: false
-//     }).done(function(data){
-//         images = data;
-//     });
-//
-//     return images;
-// }
-
-// const URL = "/gallery.json"
-// const getImages = fetch(URL)
-//     .then(response => response.json())
-//     .catch(console.error);
-
 (function($) {
     var request = $.ajax({'url': '/gallery.json'});
     request.done(function (images) {
@@ -31,5 +12,20 @@
             html += '</div>';
         });
         $('#images').html(html);
+    });
+})(jQuery);
+
+(function($) {
+    var request = $.ajax({'url': '/gallery.json'});
+    request.done(function (images) {
+        var html = '';
+        images.forEach(function(image) {
+            html += '<div>';
+            html += '<h1>' + image.imageUrl + '</h1>';
+            // html += '<p>' + image.studioName + '</p>';
+            // html += '<p>Published by ' + ad.owner.username + '</p>';
+            html += '</div>';
+        });
+        $('#curated-gallery').html(html);
     });
 })(jQuery);
