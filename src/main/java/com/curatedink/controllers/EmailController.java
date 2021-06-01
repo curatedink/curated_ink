ee5814d0f5f92e5f47b515193a6ab8b813e9cce8
javier estrada <estrada.javier.2022@gmail.com>
2021-06-01 10:41:55 -0500

    feat added functionality to my controller and my email for is now showing up

--- src/main/java/com/curatedink/controllers/EmailController.java
+++ src/main/java/com/curatedink/controllers/EmailController.java
@@ -1,30 +1,30 @@
-//package com.curatedink.controllers;
-//
-//import com.curatedink.models.Image;
-//import com.curatedink.models.User;
-//import com.curatedink.services.EmailService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//public class EmailController {
-//
-//
+package com.curatedink.controllers;
+
+import com.curatedink.models.Image;
+import com.curatedink.models.User;
+import com.curatedink.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EmailController {
+
+
 //private final EmailService emailService;
-//
-//@GetMapping("/send-email")
-//    public String welcome() {
-//
-//        return "artist-profile";
-//    }
-//
+
+@GetMapping("/send-email")
+    public String welcome() {
+
+        return "tattoos/send-email";
+    }
+
 //@PostMapping("/send-email")
 //    public String insert() {
 //
 //        return "artist-profile";
 //    }
-//}
+}
