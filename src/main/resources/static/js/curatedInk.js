7101a2668cb894ed145dd23952db154d52299fce
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-06-03 13:16:57 -0500

    Adding styling and adjusting cards on world gallery view

--- src/main/resources/static/js/curatedInk.js
+++ src/main/resources/static/js/curatedInk.js
@@ -7,12 +7,14 @@
         images.forEach(function (image) {
             let imageUserId = image.user.id;
             let profileLink = /*[[@{/profile/}]]*/'/profile/';
-            html += '<div class="card m-5" style="width: 24rem;">';
+            html += '<div class="shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">';
             html += '<div class="card-body">';
-            html += '<img class="img-thumbnail rounded mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
+            html += '<img class="img-thumbnail thumbnail mx-auto d-block" src=" ' + image.imageUrl + '"' + '/>';
             html += '</div>';
-            html += '<a class="m-2" href="' + profileLink + imageUserId + '">' + image.user.username + '</a>';
+            html += '<div class="card-footer footer">';
+            html += '<a class="button type1" href="' + profileLink + imageUserId + '">' + image.user.username + '</a>';
             // html += '<a class="btn detailed-modal" data-index-number="image.id" data-target="#gallery-detailed" data-toggle="modal"> View Details</a>'
+            html += '</div>';
             html += '</div>';
         });
         $('#images').html(html);
