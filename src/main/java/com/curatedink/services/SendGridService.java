d72ac9f6da8df508b03dca22a1945a02feb2ec6e
Michael Baker <michael.a.baker34@gmail.com>
2021-06-03 13:36:06 -0500

    add sendgrid service

---
+++ src/main/java/com/curatedink/services/SendGridService.java
@@ -0,0 +1,31 @@
+package com.curatedink.services;
+
+import com.curatedink.models.*;
+import com.sendgrid.*;
+
+import java.io.IOException;
+
+public class SendGridService {
+    SendGrid sendGrid;
+
+    public SendGridService(SendGrid sendGrid) {
+        this.sendGrid = sendGrid;
+    }
+
+    public Response sendEmail(String from, String to, String subject, Content body) {
+        Mail mail = new Mail(new Email(from), subject, new Email(to), body);
+        mail.setReplyTo(new Email("curated.ink.com@gmail.com"));
+        Request request = new Request();
+        Response response = null;
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            this.sendGrid.api(request);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return response;
+    }
+}
